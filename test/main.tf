6e21833991ff410382bfa6cdc8190c8b0bee1355
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-09-05 17:06:40 +0300

    test2

---
+++ test/main.tf
@@ -0,0 +1,8 @@
+module "test_vpc" {
+  source = "git::https://github.com/Nexora-Banking-App/infra-modules.git//vpc?ref=main"
+
+  environment     = "test"
+  vpc_cidr        = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
