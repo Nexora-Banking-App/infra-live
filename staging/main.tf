b9eb941afa1c6d1607509920cd7640da4c8c111c
ahmeddhussain <ahmedkhater2611@gmail.com>
2026-09-07 14:40:19 +0300

    fix version

--- staging/main.tf
+++ staging/main.tf
@@ -3,7 +3,7 @@ module "staging_eks" {
   source = "git::https://github.com/Nexora-Banking-App/infra-modules.git//eks?ref=main"
 
   cluster_name        = "nexora-staging"
-  cluster_version     = "1.35" 
+  cluster_version     = "1.31"
   environment         = "staging"
   vpc_id              = data.terraform_remote_state.shared.outputs.vpc_id
   subnet_ids          = data.terraform_remote_state.shared.outputs.private_subnets
@@ -12,6 +12,7 @@ module "staging_eks" {
   min_size            = 1
   max_size            = 3
 }
+
 # 2. Independent Staging Database (Single-AZ to save cost)
 module "staging_rds" {
   source = "git::https://github.com/Nexora-Banking-App/infra-modules.git//rds?ref=main"
@@ -22,5 +23,5 @@ module "staging_rds" {
   eks_security_group_id   = module.staging_eks.cluster_security_group_id
   instance_class          = "db.t3.micro"
   multi_az                = false
-  backup_retention_period = 1 # <-- Complies with Free Tier limits
+  backup_retention_period = 1
 }
